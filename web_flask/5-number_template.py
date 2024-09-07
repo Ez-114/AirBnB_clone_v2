64c2d5852a68658964da62f43894bcfd54f842d2
Ez-114 <morganezz226@gmail.com>
2024-09-07 03:34:26 +0300

    [add] task 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""start a web server"""
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """display hello text"""
+
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """goes to hbnb"""
+
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """display c followed by passed text"""
+
+    return "C " + text.replace('_', ' ')
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pyiscool(text="is cool"):
+    """Display text"""
+
+    return "Python " + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def isanum(n):
+    """display a text that validate a passed variable is an int"""
+
+    return "{:d} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def rndr_for_nums(n):
+    """render a HTML page if passed arg is an int"""
+
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
